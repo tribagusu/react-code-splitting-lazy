663c4aa4c9a5bbd4163a90fecf27bee5000008c9
tribagus <tri.bagus21@gmail.com>
2022-10-24 16:32:46 +0700

    setup chunk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [names, setNames] = useState(null)
+
+  const onLoad = () => {
+    import("./names").then((module) => {
+      setNames(module.default)
+
+      import("./utilities" /* webpackChunkName: "utilities" */).then(
+        ({ makeUpperCase }) => {
+          setNames((names) => makeUpperCase(names))
+        }
+      )
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Home App</div>
+      <button onClick={onLoad}>Load</button>
+      <div>{JSON.stringify(names)}</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
